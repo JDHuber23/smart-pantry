bea66dc6a5920957e84659022fd9d27683af429f
Jacob Huber <jacobhuber@iMac.local>
2021-05-01 12:27:12 -0700

    initial ios project commit

---
+++ Smart Pantry/Smart Pantry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Pantry
+//
+//  Created by Jacob Huber on 5/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
