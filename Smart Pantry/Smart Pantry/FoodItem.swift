75835d146659c51c7f40c25ac31043ea20db9cc0
Jacob Huber <jacobhuber@iMac.local>
2021-05-05 01:15:15 -0700

    added account view, recipes view, and food item fields

--- Smart Pantry/Smart Pantry/FoodItem.swift
+++ Smart Pantry/Smart Pantry/FoodItem.swift
@@ -10,9 +10,23 @@ import Foundation
 class FoodItem {
     
     var name: String
+    var amount: Int
+    var location: String
+    var carbohydrates: Int
+    var protein: Int
+    var sugar: Int
+    var fat: Int
+    var sodium: Int
     
-    init(name: String) {
+    init(name: String, amount: Int, location: String, carbohydrates: Int, protein: Int, sugar: Int, fat: Int, sodium: Int) {
         self.name = name
+        self.amount = amount
+        self.location = location
+        self.carbohydrates = carbohydrates
+        self.protein = protein
+        self.sugar = sugar
+        self.fat = fat
+        self.sodium = sodium
     }
     
 }
--- Smart Pantry/Smart Pantry/ViewController.swift
+++ Smart Pantry/Smart Pantry/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     // food items
-    var food: [String: Int] = [:]
+    var food: [FoodItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +18,30 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func addFood(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Food Item", message: "Enter the name, amount, and nutritional values.", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Name"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Amount"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Carbohydrates"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Sodium"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Fat"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Protein"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Sugar"
+        }
+        
+    }
 }
 
